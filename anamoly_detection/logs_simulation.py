3351138b5c1ddcde6a2b5b75b106a2abd2cb714d
HARIHARUTO <bujjihari16@gmail.com>
2025-12-11 15:23:50 +0530

    finally generating logs are required

--- anamoly_detection/logs_simulation.py
+++ anamoly_detection/logs_simulation.py
@@ -2,16 +2,19 @@ import os
 import random
 import time 
 import json
+from datetime import datetime
 
 def simulate_logs():
     log_dir='logs'
     log_file=os.path.join(log_dir,'app.log')
     if not os.path.exists(log_dir):
-               os.makedirs(log_dir)
+       os.makedirs(log_dir)
     
     ROUTES=['/Home','/login','/search','/page','/checkout']
     anamoly_probabiltiy=0.03
-    while True:
+    max_logs=10
+    count=0
+    while count<max_logs:
         
         is_anamoly=random.random()<anamoly_probabiltiy
         if is_anamoly:
@@ -23,12 +26,14 @@ def simulate_logs():
              status_code=random.randint(100,300)
              route=random.choice(ROUTES)
         log_entry={
-            'timestamp'
-            'latency_ms'"latency",
+            'timestamp':datetime.now().isoformat(),
+            'latency_ms':latency,
             'status_code':status_code,
             'route':route
         }
-        with open(log_file,'a')as f
+        with open(log_file,'a')as f:
+            f.write(json.dumps(log_entry)+'\n')
+        count+=1
         
         time.sleep(random.uniform(0.1,0.5))
 if __name__=="__main__":
